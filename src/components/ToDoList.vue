e4f1df510257c38b7e75fec27cec2202af40e3d1
Karli Juhkam <karli.juhkam@gmail.com>
2024-09-25 18:03:47 +0300

    add loader and API error handling

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="q-pa-md q-gutter-md">
-    <q-list bordered padding class="rounded-borders" style="max-width: 650px">
+    <q-list bordered padding class="rounded-borders" style="min-width: 650px; max-width: 650px">
       <div class="q-px-md">
-        <q-input borderless v-model="inputText" label="Add a new to-do">
+        <q-input borderless v-model="inputText" label="Add a new to-do" @keydown.enter="addNewToDo(inputText)">
           <template v-slot:after>
             <q-btn color="primary" label="Add" @click="addNewToDo(inputText)" />
           </template>
@@ -13,11 +13,14 @@
         <q-item-section>
           <q-checkbox size="lg" v-model="toDo.completed" val="lg" :label="toDo.title" />
         </q-item-section>
-
         <q-item-section side>
           <q-btn round flat color="deep-orange" icon="delete" @click="removeToDo(toDo.id)" />
         </q-item-section>
       </q-item>
+      <q-inner-loading :showing="loading" />
+      <p v-if="toDoList && toDoList.length === 0" class="q-pa-md text-center">
+        No to-dos found. Add a new to-do above.
+      </p>
     </q-list>
   </div>
 </template>
@@ -25,13 +28,29 @@
 <script setup>
 import { ref } from 'vue';
 const toDoList = ref(null);
+const loading = ref(false);
 
-// Fetch to-do list from JSONPlaceholder API
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(data => {
-    toDoList.value = data;
-  });
+const loadInitialData = async () => {
+  try {
+    loading.value = true;
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => {
+        toDoList.value = data;
+      });
+    if (response && !response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    toDoList.value = await response.json();
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    loading.value = false;
+  }
+}
+
+loadInitialData();
 
 const inputText = ref(null);
 
