80dc610b8e2279ac0338df8f805182e9fa13cb20
Karli Juhkam <karli.juhkam@gmail.com>
2024-09-25 21:23:36 +0300

    add to-do filtering functionality

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,34 +1,46 @@
 <template>
-  <div class="q-pa-md q-gutter-md">
-    <q-list bordered padding class="rounded-borders" style="min-width: 650px; max-width: 650px">
-      <div class="q-px-md">
-        <q-input borderless v-model="inputText" label="Add a new to-do" @keydown.enter="addNewToDo(inputText)">
-          <template v-slot:after>
-            <q-btn color="primary" label="Add" @click="addNewToDo(inputText)" />
-          </template>
-        </q-input>
-      </div>
-      <q-separator spaced />
-      <q-item v-for="toDo in toDoList" :key="toDo.id" clickable v-ripple>
+  <div class="column q-pa-md q-gutter-md full-width full-height">
+    <div class="q-px-md">
+      <q-input v-model="inputText" label="Add a new to-do" @keydown.enter="addNewToDo(inputText)">
+        <template v-slot:after>
+          <q-btn color="primary" label="Add" @click="addNewToDo(inputText)" />
+        </template>
+      </q-input>
+    </div>
+    <q-virtual-scroll v-if="filteredToDos && filteredToDos.length > 0" bordered class="rounded-borders"
+      :items="filteredToDos" v-slot="{ item, index }" style="height: 75dvh">
+      <q-item :key="index" clickable v-ripple>
         <q-item-section>
-          <q-checkbox size="lg" v-model="toDo.completed" val="lg" :label="toDo.title" />
+          <q-checkbox size="lg" v-model="item.completed" val="lg" :label="item.title" />
         </q-item-section>
         <q-item-section side>
-          <q-btn round flat color="deep-orange" icon="delete" @click="removeToDo(toDo.id)" />
+          <q-btn round flat color="deep-orange" icon="delete" @click="removeToDo(item.id)" />
         </q-item-section>
       </q-item>
       <q-inner-loading :showing="loading" />
-      <p v-if="toDoList && toDoList.length === 0" class="q-pa-md text-center">
-        No to-dos found. Add a new to-do above.
-      </p>
-    </q-list>
+    </q-virtual-scroll>
+    <p v-else class="q-pa-md text-center flex content-center justify-center" style="height: 75dvh">
+      {{ noToDoMessage }}
+    </p>
+    <div>
+      <q-btn-toggle v-model="filter" toggle-color="primary" :options="[
+        { label: 'Show all', value: 'all' },
+        { label: 'Done', value: 'done' },
+        { label: 'Undone', value: 'undone' }
+      ]" />
+
+      <q-btn outline color="red" label="Clear all" icon="delete" @click="clearAll" />
+      <q-btn outline color="primary" icon="refresh" @click="reloadToDoList" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 const toDoList = ref(null);
 const loading = ref(false);
+const inputText = ref(null);
+const filter = ref('all');
 
 const loadInitialData = async () => {
   try {
@@ -38,11 +50,13 @@ const loadInitialData = async () => {
       .then(data => {
         toDoList.value = data;
       });
-    if (response && !response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    if (response) {
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      toDoList.value = await response.json();
     }
 
-    toDoList.value = await response.json();
   } catch (error) {
     console.error(error.message);
   } finally {
@@ -51,8 +65,6 @@ const loadInitialData = async () => {
 }
 
 loadInitialData();
-
-const inputText = ref(null);
 
 const addNewToDo = (newToDoTitle) => {
   // Prevent adding empty to-do
@@ -63,7 +75,7 @@ const addNewToDo = (newToDoTitle) => {
     userId: 1,
     id: toDoList.value.length + 1,
     title: newToDoTitle,
-    completed: false
+    completed: filter.value === 'done' ? true : false
   };
   toDoList.value = [newToDo, ...toDoList.value];
 
@@ -75,5 +87,36 @@ const removeToDo = (id) => {
   toDoList.value = toDoList.value.filter(toDo => toDo.id !== id);
 };
 
+const filterToDos = (filter) => {
+  switch (filter) {
+    case 'done':
+      return toDoList.value.filter(toDo => toDo.completed);
+    case 'undone':
+      return toDoList.value.filter(toDo => !toDo.completed);
+    default:
+      return toDoList.value;
+  }
+};
+
+const filteredToDos = computed(() => filterToDos(filter.value));
+
+const clearAll = () => {
+  toDoList.value = [];
+};
+
+const noToDoMessage = computed(() => {
+  if (filter.value === 'done') {
+    return 'No to-dos with "Done" status found. Add a new to-do above.';
+  }
+  if (filter.value === 'undone') {
+    return 'No to-dos with "Undone" status found. Add a new to-do above.';
+  }
+
+  return ' No to-dos found. Add a new to-do above.';
+});
+
+const reloadToDoList = () => {
+  loadInitialData();
+}
 
 </script>
